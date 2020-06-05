0892bc56ffdbb9186e56a34222024c718082ac5f
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2020-06-04 19:47:07 -0600

    snapshot

--- src/com/radiometrics/plugin/rdx.sql
+++ src/com/radiometrics/plugin/rdx.sql
@@ -3,16 +3,30 @@
 -----------------------------------------------------------------------
 CREATE TABLE rdx_notifications (entry_id varchar(200),
                   		   event_type varchar(200),
-				   date ramadda.datetime); 
+				   date ramadda.datetime);
+
+CREATE TABLE rdx_instrument_status_log (
+       entry_id varchar(200),
+       date ramadda.datetime,
+       instrument_id varchar(200),
+       last_network_connection ramadda.datetime,
+       last_data ramadda.datetime,
+       network_up int,
+       data_down int
+);
+
+
 
 --- alter table rdx_notifications add column description varchar(5000);
+
+drop table rdx_test_instrument_status;
 
 CREATE TABLE rdx_test_instrument_status (
        instrument_id varchar(200),
        type varchar(200),
        last_network_connection ramadda.datetime,
        last_data_time ramadda.datetime,
-       network_is_up int,
+       network_up int,
        data_down int
 );
 
