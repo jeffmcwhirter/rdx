48b91d04224c9b9ab6726b788a3c8ebae12f9b89
Jeff McWhirter <jeff.mcwhirter@gmail.com>
2020-06-22 21:16:10 -0600

    search exact ID for instruments

--- src/com/radiometrics/plugin/RdxApiHandler.java
+++ src/com/radiometrics/plugin/RdxApiHandler.java
@@ -1,6 +1,17 @@
 /*
-* Copyright (c) 2020 Radiometrics Inc.
+* Copyright (c) 2008-2019 Geode Systems LLC
 *
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*     http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
 */
 
 package com.radiometrics.plugin;
@@ -1290,13 +1301,18 @@ public class RdxApiHandler extends RepositoryManager implements RdxConstants,
         Request tmpRequest = getRepository().getTmpRequest();
         tmpRequest.put(ARG_TYPE, instrument.typeId);
         String id = instrument.siteId;
-        tmpRequest.put("search.rdx_instrument.instrument_id", id);
+        tmpRequest.put("search.rdx_instrument.instrument_id",
+                       "\"" + id + "\"");
         List[]      result  = getEntryManager().getEntries(tmpRequest);
         List<Entry> entries = new ArrayList<Entry>();
         entries.addAll((List<Entry>) result[0]);
         entries.addAll((List<Entry>) result[1]);
         if (entries.size() == 0) {
             return null;
+        }
+        //Should only be one
+        if (entries.size() > 1) {
+            log("Warning: More than one instruments for:" + id);
         }
 
         return entries.get(0);
